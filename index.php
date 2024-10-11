0373805beca4decd05bb071388355e2912884976
Lavina-Roshni <lavimanoj2603@gmail.com>
2024-10-12 01:48:58 +0530

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $server_name="db";
 $username="root";
-$password="";
+$password="root";
 $database_name="tnpothole";
 
 $conn = mysqli_connect($server_name,$username,$password,$database_name);
