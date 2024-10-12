1accdc4b2b6ddf4da380d00d0a9dfe30f7f8c834
Lavina-Roshni <lavimanoj2603@gmail.com>
2024-10-12 23:44:57 +0530

    index.php

--- index.php
+++ index.php
@@ -1,19 +1,8 @@
 <?php
-$server_name="db";
-$username="root";
-$password="root";
-$database_name="tnpothole";
+$conn = mysqli_connect("db", "root", "root", "tnpothole"); // Use actual database name
 
-$conn = mysqli_connect($server_name,$username,$password,$database_name);
-//now check the connection
-if(!$conn)
-{
-	die("Connection Failed:" . mysqli_connect_error());
-
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
 }
-if($conn)
-{
-	die("Connection executed");
-
-}
+echo "Connected successfully";
 ?>
